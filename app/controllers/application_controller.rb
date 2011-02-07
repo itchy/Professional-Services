5e1de5da03c72c97b3c8fa1d096b2794e83b5694
Scott Johnson <7.scott.j@gmail.com>
2011-02-07 00:34:50 -0600

    added some copy and pics for evaluation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :defaults
+  
+  def defaults
+    date = Date.today
+    @footer = "NewSmileTulsa.com " + Date.today.strftime("%m/%d/%Y")
+  end
+  
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS[:pretty] = "%m/%d%Y"
