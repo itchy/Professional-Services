1aa46e5fcd1ce3cc0896ac6e86c08767938598e1
Scott Johnson <7.scott.j@gmail.com>
2011-02-08 22:13:03 -0600

    add js scoller to services

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(document).ready(function() {
-  // alert('Welcome!');    
-  // $('div#map_canvas').goMap({
-  //    zoom: 10,
-  //    maptype: 'ROADMAP',
-  //    markers: [
-  //      {address: '2860 Duffton Loop, Tallahassee, FL 32303', icon: '../img/map-green.png', html: '2860 Duffton Loop'},
-  //    ],
-  //    hideByClick: true});
-  $('form.html5').html5form_custom();
-  $('div#map_canvas').get_map();
-  
-  // alert('Welcome!');    
-  
-  
-});
-
 
 $.fn.extend({
 
- get_map: function() {    
+ get_map: function() { 
+   alert('Welcome!');    
   var $this = $(this);
   var center_latlng = new google.maps.LatLng(36.104312, -95.9260086);
   var office_latlng = new google.maps.LatLng(36.103771, -95.941394);
@@ -40,7 +24,34 @@ $.fn.extend({
        map: map, 
        title:"Hello World!"
    });
-    
+  
   }
 
-});  
+});
+
+
+$(document).ready(function() {
+  // alert('Welcome!');    
+  // $('div#map_canvas').goMap({
+  //    zoom: 10,
+  //    maptype: 'ROADMAP',
+  //    markers: [
+  //      {address: '2860 Duffton Loop, Tallahassee, FL 32303', icon: '../img/map-green.png', html: '2860 Duffton Loop'},
+  //    ],
+  //    hideByClick: true});
+  $('form.html5').html5form_custom();
+  // THIS IF STATMENT IS NEEDED for some reason -- SSJ 
+  if ($('div#map_canvas').length > 0){ 
+    $('div#map_canvas').get_map();
+  }  
+  $('ul#banner_treatments').list_ticker({
+  		speed: 4000,
+      effect: 'fade'
+  	}); 
+  
+  
+});
+
+
+
+  
